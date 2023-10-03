171993e4947ff4d81cff603aac7de29093ff4e84
sudarshan4718 <146313337+sudarshan4718@users.noreply.github.com>
2023-10-03 23:08:49 +0530

    Update index.js

--- AnswerVoyage/SignUpBackend/index.js
+++ AnswerVoyage/SignUpBackend/index.js
@@ -1,20 +1,15 @@
-import express from "express"
-import cors from "cors"
-import mongoose from "mongoose"
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(cors())
+const app = express();
+const port = 9000;
 
-// mongoose.connect("localhost://27017/avdb" , {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }, () => {
-//     console.log("DB Connected")
-// })
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-
+// Function to connect to the MongoDB database
 async function connectToDatabase() {
   try {
     await mongoose.connect('mongodb://localhost:27017/AvRegister', {
@@ -30,82 +25,51 @@ async function connectToDatabase() {
 connectToDatabase();
 
 const userSchema = new mongoose.Schema({
-  name : String,
-  email : String,
-  password : String
-})
+  name: String,
+  email: String,
+  password: String,
+});
 
-const User = new mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
 
-
-//Routes
-app.post("/login", (req,res) => {
-  const {email, password} =req.body
-  User.findOne({email : email}, (err,user) => {
-    if(user){
-      if(password === user.password) {
-        res.send({message: "Login Successfull", user: user})
+    if (user) {
+      if (password === user.password) {
+        res.json({ message: "Login Successful", user: user });
+      } else {
+        res.json({ message: "Password incorrect" });
       }
-      else{
-        res.send({message:"Password incorrect"})
-      }
+    } else {
+      res.json({ message: "User not registered" });
     }
-    else{
-      res.send("User not registered")
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      res.json({ message: "User already registered" });
+    } else {
+      const newUser = new User({ name, email, password });
+      await newUser.save();
+      res.json({ message: "Successfully Registered, please login now" });
     }
-  })
-})
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
-app.post("/register", (req,res) => {            
-   const {name, email, password} =req.body
-   User.findOne({email: email},(err,user) => {         // If email exists then
-      if(user){
-        res.send({message : "User already registered"})
-      }
-      else {
-        const user = new User({
-          name,
-          email,
-          password
-         })
-         user.save( err => {                  // Saving the users info in database
-          if(err) {
-            res.send(err)
-          }
-          else {
-            res.send({message: "Successfully Registered , Please login now"})
-          }
-         })
-      }
-   })
-   
-})
-
-// app.get("/", (req,res) => {
-//   res.send("My API");
-// })
-
-// const uri = 'mongodb://127.0.0.1:27017/AvLogin';
-
-// mongoose.connect(uri, {
-//   serverSelectionTimeoutMS: 5000
-// }).catch(err => console.log(err.reason));
-
-app.listen(9000,() => {
-    console.log("BE started at port 9000")
-})
-
-// const mongoose = require("mongoose");
-
-
-// mongoose.connect("mongodb://127.0.0.1:27017/SignUp", {
-//     useNewUrlParser:true ,
-//     useUnifiedTopology:true
-// }).then(() => {
-//     console.log(`Connection Successful`);
-
-// }).catch((e) =>{
-//     console.log(`No Connection.`)
-// })
-
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
