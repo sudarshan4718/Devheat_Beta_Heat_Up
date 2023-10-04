60636ef069eb206ef320b79e426086c0bde77e34
sudarshan4718 <146313337+sudarshan4718@users.noreply.github.com>
2023-10-04 18:07:43 +0530

    Update Contacts.js

--- AnswerVoyage/signuppage/src/components/Contacts/Contacts.js
+++ AnswerVoyage/signuppage/src/components/Contacts/Contacts.js
@@ -1,1 +1,28 @@
+import React from 'react';
+import "./Contacts.css"
+import contactimage from "./QA.png";
+
+const Contacts = () => {
+  return (
+    <div className="contacts-container">
+      <h1>Answer Voyage</h1>
+      <div className="contact-info">
+        <div className="contact">
+          <h2>Sudarshan Jha</h2>
+          <p>Email: sudarshanjha2004@gmnail.com</p>
+          <p>Phone: +91 9004115437</p>
+        </div>
+        <div className="contact">
+          <h2>Vikesh Kumar</h2>
+          <p>Email: vikesh3640@gmail.com</p>
+          <p>Phone: +91 7903288699</p>
+        </div>
+      </div>
+      <p className='contacts-address'>Address: IIIT Surat, Kamrej, Surat</p>
+      <img src={contactimage} className="contact-image"></img>
+    </div>
+  );
+};
+
+export default Contacts;
 
