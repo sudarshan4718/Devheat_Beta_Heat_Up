a5905f265918c3744d90a26da82e974b35b7d688
sudarshan4718 <146313337+sudarshan4718@users.noreply.github.com>
2023-10-04 18:02:16 +0530

    Update homepage.js

--- AnswerVoyage/signuppage/src/components/homepage/homepage.js
+++ AnswerVoyage/signuppage/src/components/homepage/homepage.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import "./homepage.css";
+import Body from './Body';
+import Contacts from "../Contacts/Contacts";
+import { NavLink} from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
 
@@ -16,91 +19,22 @@ const Homepage = ({ setLoginUser }) => {
       <header>
         <h1>
           AnswerVoyage &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <img src="websitelogo.png" alt="Website Logo" />
+          
         </h1>
       </header>
-      <nav>
-        <ul>
-          {/* Navigation list items */}
-          {/* ... Your navigation code ... */}
-          <li><a href="#" title="Home"><i class="bi bi-house"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
-          </svg></a></li>
-
-     <li><a href="#askqn" title="Post Question"><i class="bi bi-upload"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16">
-        <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-        <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z"/>
-        </svg></a>
-
-      </li>
-      <li><a href="#" title="Communitites"><i class="bi bi-people"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
-          <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8Zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022ZM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816ZM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4Z"/>
-          </svg></a></li>
-
-      <li><a href="#contacts" title="Contacts"><i class="bi bi-telephone"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
-          <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-          </svg></a></li>
-
-      <li><a href="#" title="Notifications">
-           <i class="bi bi-bell"></i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
-           <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/></svg>
-          </a>
-      </li>
-      <div className="button" onClick={() => setLoginUser({}) }>Logout</div>
-        </ul>
-      </nav>
-      <br />
-      <form >
-  <div>
-    <label htmlFor="questionInput">
-      <p id="xyz">&nbsp;&nbsp;Ask a Question</p>
-    </label>
-    &nbsp;
-    <input
-      type="text"
-      id="questionInput"
-    />
-    <div className="submitbutton" onClick={() => navigate("/post-question") }>
-      &nbsp;&nbsp;<b>Post a Question</b>
-      <input type="submit" value="Submit" name="submitkrnahai" />
-    </div>
-  </div>
-  <br />
-</form>
-
       
-      <h2>Stories</h2>
-      <div className="row">
-        {/* ... Your story content ... */}
-      </div>
-      Upvote
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
-      <i className="fa fa-thumbs-up"></i>
-      <br />
-      <br />
-      {/* Repeat the story content and upvote button as needed */}
-      <div>
-        View more....
-        <br />
-        <br />
-        <br />
-      </div>
-      <h3>&nbsp;Contacts</h3>
-      <address className="address1" id="contacts">
-        &nbsp;Managed by AnswerVoyage Pvt Limited.
-        <br />
-        &nbsp;Visit us at:<br />
-        &nbsp;vikeshkumar3640@gmail.com<br />
-        &nbsp;Contact: 7903288699
-        &nbsp;Valthan, Surat, Gujarat<br />
-        &nbsp;India
-      </address>
-      <div className="button" onClick={() => setLoginUser({})}>
-        Logout
-      </div>
+
+    <nav className="navbar">
+      
+      <ul className="navbar-list">
+        <li><a href="/homepage">Home</a></li>
+        <li><NavLink to="/contacts">Contacts</NavLink></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/logout" onClick={() => setLoginUser({}) }>Logout</a></li>
+      </ul>
+    </nav>
+      
+    <Body/>
     </div>
   );
 };
