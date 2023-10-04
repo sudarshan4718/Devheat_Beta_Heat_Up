ee76f7627bd53cbeb4b1a0eaeb323a2c77fdc716
sudarshan4718 <146313337+sudarshan4718@users.noreply.github.com>
2023-10-04 14:37:56 +0530

    Update App.js

--- AnswerVoyage/signuppage/src/App.js
+++ AnswerVoyage/signuppage/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import Login from "./components/login/login"
-import Register from "./components/register/register"
-import Homepage from "./components/homepage/homepage" // Import Homepage
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useState } from 'react';
+import Homepage from "./components/homepage/homepage";
+import PostQuestion from "./components/post-question/post-question"; // Import PostQuestion
+import Login from "./components/login/login";
+import Register from "./components/register/register";
+
 
 function App() {
   const [user, setLoginUser] = useState({});
@@ -12,9 +13,16 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/login' element={user && user._id ? <Navigate to='/' /> : <Login setLoginUser={setLoginUser} />} />
-          <Route path='/register' element={user && user._id ? <Navigate to='/' /> : <Register />} />
-          <Route path='/' element={user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />} />
+          <Route path="/login" element={user && user._id ? <Navigate to="/" /> : <Login setLoginUser={setLoginUser} />} />
+          <Route path="/register" element={user && user._id ? <Navigate to="/" /> : <Register />} />
+
+          {/* Route for posting questions */}
+          <Route path="/post-question" element={<PostQuestion />} />
+
+          <Route
+            path="/"
+            element={user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser} />}
+          />
         </Routes>
       </Router>
     </div>
