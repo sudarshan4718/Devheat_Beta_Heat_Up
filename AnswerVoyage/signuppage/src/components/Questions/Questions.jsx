46886148249b2d52972451a443b2490df69cabf7
sudarshan4718 <146313337+sudarshan4718@users.noreply.github.com>
2023-10-06 02:18:54 +0530

    Update Questions.jsx

--- AnswerVoyage/signuppage/src/components/Questions/Questions.jsx
+++ AnswerVoyage/signuppage/src/components/Questions/Questions.jsx
@@ -1,1 +1,37 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+
+ const Questions =() =>{
+    const [data,setData] = useState([]);
+            axios.get('http://localhost:9000/Questions')
+            .then((response)=>{
+                setData(response.data);
+                // data = response.data
+                console.log(response.data);
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+
+            
+
+    return (
+        <>
+        <div>
+         <h1> See Questions Asked Here</h1>
+         <ul> {data.map((item)=>(
+            <li key={item._id}>{item.questionText}</li>
+         ))
+            } </ul>
+        </div>
+      </>
+       );
+
+    
+ };
+
+export default Questions;
 
