bb343c02daa3d19235a9e7f43a2c0054c325c1a2
sudarshan4718 <146313337+sudarshan4718@users.noreply.github.com>
2023-10-06 05:44:06 +0530

    Update Questions.jsx

--- AnswerVoyage/signuppage/src/components/Questions/Questions.jsx
+++ AnswerVoyage/signuppage/src/components/Questions/Questions.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./Questions.css";
+import { NavLink} from 'react-router-dom';
 
 
- const Questions =() =>{
+ const Questions =({ setLoginUser }) =>{
     const [data,setData] = useState([]);
             axios.get('http://localhost:9000/Questions')
             .then((response)=>{
@@ -24,6 +25,16 @@ import "./Questions.css";
         <h1 className="header1">AnswerVoyage
         </h1>
     </header>
+    <nav className="navbar">
+      
+      <ul className="navbar-list">
+        <li><a href="/homepage">Home</a></li>
+        <li><NavLink to="/contacts">Contacts</NavLink></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/logout" onClick={() => setLoginUser({}) }>Logout</a></li>
+        <li><a href="/Questions">Questions</a></li>
+      </ul>
+    </nav>
         <div className="heading container">
          <h1 className="heading1"> See Questions Asked Here</h1>
          <br/>
